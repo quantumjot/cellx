7e70e9174352ab86a8813e493266dcafb7c828d7
quantumjot <code@arlowe.co.uk>
2020-10-23 11:32:54 +0100

    updated io docstrings

--- cellx/tools/io.py
+++ cellx/tools/io.py
@@ -1,12 +1,13 @@
-import os
-import json
 import hashlib
+import json
+import os
+
 import numpy as np
 from skimage import io
 
 
 def _hash_encoding(x: np.array):
-    """ create a hash of the encoding """
+    """Create a unique hash of the encoding."""
     # return hash(x.tostring())
     m = hashlib.sha256()
     m.update(x.tostring())
@@ -14,40 +15,42 @@ def _hash_encoding(x: np.array):
 
 
 class EncodingWriter:
-    """ EncodingWriter
+    """EncodingWriter.
 
-    Handler class for writing encoded data
+    Handler class for writing encoded data.
 
-    Params:
-        filename: str, a path and filename for the json file storing the
-                  metadata
+    Parameters
+    ----------
+    filename : str
+        A path and filename for the json file storing the metadata.
 
-    Usage:
+    Usage
+    -----
 
-        with EncodingWriter('/path/to/encodings.json') as writer:
+    with EncodingWriter('/path/to/encodings.json') as writer:
 
-            # put your code here to generate the encoding
-            src_file = 'GV0800/Pos12/data.tif'
-            encoding = some_function_to_generate_encoding(src_file)
+        # put your code here to generate the encoding
+        src_file = 'GV0800/Pos12/data.tif'
+        encoding = some_function_to_generate_encoding(src_file)
 
-            # save the encoding as a destination file:
-            dst_file = 'GV0800/Pos12/data_encoded.npz'
+        # save the encoding as a destination file:
+        dst_file = 'GV0800/Pos12/data_encoded.npz'
 
-            # store metadata, e.g. model parameters used for encoding
-            metadata = {'model': 'my_cool_model.h5',
-                        'version': '0027'}
+        # store metadata, e.g. model parameters used for encoding
+        metadata = {'model': 'my_cool_model.h5',
+                    'version': '0027'}
 
-            writer.write(encoding,
-                         src_file,
-                         dst_file,
-                         class_label=0,
-                         metadata=metadata)
+        writer.write(encoding,
+                     src_file,
+                     dst_file,
+                     class_label=0,
+                     metadata=metadata)
 
     """
-    def __init__(self,
-                 filename: str):
 
-        assert filename.endswith('.json')
+    def __init__(self, filename: str):
+
+        assert filename.endswith(".json")
 
         # check that the path to the filename exists
         path, _ = os.path.split(filename)
@@ -63,52 +66,57 @@ class EncodingWriter:
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        with open(self._filename, 'w') as file:
-            json.dump(self._json_data, file, separators=(',', ':'), indent=2)
+        with open(self._filename, "w") as file:
+            json.dump(self._json_data, file, separators=(",", ":"), indent=2)
 
-    def write(self,
-              encoding: np.ndarray,
-              src_file: str,
-              dst_file: str,
-              class_label: int = 0,
-              metadata: dict = {}):
-        """ write out the encoding """
+    def write(
+        self,
+        encoding: np.ndarray,
+        src_file: str,
+        dst_file: str,
+        class_label: int = 0,
+        metadata: dict = {},
+    ):
+        """Write out the encoding."""
 
-        assert dst_file.endswith('.npz')
+        assert dst_file.endswith(".npz")
 
         # save the npz file
         np.savez(dst_file, encoding=encoding, class_label=class_label)
 
         # save out the data to the json dictionary
-        data = {'dst_file': dst_file,
-                'src_file': src_file,
-                'class_label': class_label,
-                'hash': _hash_encoding(encoding)}
+        data = {
+            "dst_file": dst_file,
+            "src_file": src_file,
+            "class_label": class_label,
+            "hash": _hash_encoding(encoding),
+        }
         self._json_data[dst_file] = {**data, **metadata}
 
 
 class EncodingReader:
-    """ EncodingReader
+    """EncodingReader.
 
-    Handler class for reading encoded data
+    Handler class for reading encoded data. This compares the hash of the loaded
+    encoding with that stored in the metadata from the json. this is to ensure
+    that the metadata and the encoding match.
 
-    Params:
-        filename: str, a path to the json file created by the EncodingWriter
+    Parameters
+    ----------
+    filename : str
+        A path and filename for the json file storing the metadata.
 
-    Usage:
-        encodings = EncodingReader('/path/to/encodings.json')
-        for encoded, metadata in encodings:
-            print(encoding, metadata)
+    Usage
+    -----
+    encodings = EncodingReader('/path/to/encodings.json')
+    for encoded, metadata in encodings:
+        print(encoding, metadata)
 
-    Notes:
-        this compares the hash of the loaded encoding with that stored in the
-        metadata from the json. this is to ensure that the metadata and the
-        encoding match.
     """
-    def __init__(self,
-                 filename: str):
+
+    def __init__(self, filename: str):
         # grab the data from the file
-        with open(filename, 'r') as file:
+        with open(filename, "r") as file:
             data = json.load(file)
 
         # recover the entries in the dictionary
@@ -127,38 +135,36 @@ class EncodingReader:
         return self[self._idx - 1]
 
     def __len__(self):
-        """ return the number of entries """
+        """Return the number of entries."""
         return len(self._metadata)
 
     def __getitem__(self, idx: int):
+        """Get and encoding and metadata."""
         metadata = self._metadata[idx]
 
         # load the encoding
-        encoded = np.load(metadata['dst_file'])
-        encoding = encoded['encoding']
+        encoded = np.load(metadata["dst_file"])
+        encoding = encoded["encoding"]
 
         # sanity check that the class label is correct and that the hash match
-        assert encoded['class_label'] == metadata['class_label']
-        assert _hash_encoding(encoding) == metadata['hash']
+        assert encoded["class_label"] == metadata["class_label"]
+        assert _hash_encoding(encoding) == metadata["hash"]
         return encoding, metadata
 
-    def load_image(self,
-                   idx: int,
-                   scale: int = 1,
-                   use_cutoff: bool = True):
-        """ get the associated image data """
+    def load_image(self, idx: int, scale: int = 1, use_cutoff: bool = True):
+        """Get the associated image data."""
         metadata = self._metadata[idx]
 
         # load the raw image data
-        image_fn = metadata['src_file']
-        if not image_fn.endswith('.tif'):
-            image_fn = image_fn + '.tif'
+        image_fn = metadata["src_file"]
+        if not image_fn.endswith(".tif"):
+            image_fn = image_fn + ".tif"
 
         image = io.imread(image_fn)
 
         # get the cutoff
         if use_cutoff:
-            cutoff = metadata['cutoff']
+            cutoff = metadata["cutoff"]
         else:
             cutoff = image.shape[0]
 
