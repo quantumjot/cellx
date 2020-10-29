4d05461e86a34ee9c8777a3c7900ba7763a36c9a
Kristina Ulicna <kristinaulicna@eduroam-int-dhcp-97-106-218.ucl.ac.uk>
2020-10-29 21:04:04 +0000

    Defined CNNClassifier class

--- cellx/networks/classifier.py
+++ cellx/networks/classifier.py
@@ -1,1 +1,8 @@
-# this will be the classifier
+# This will be the CNN classifier:
+
+import tensorflow as tf
+
+inputs = tf.keras.Input(shape=(2,))
+x = tf.keras.layers.Dense(4, activation=tf.nn.relu)(inputs)
+outputs = tf.keras.layers.Dense(4, activation=tf.nn.softmax)(x)
+model = tf.keras.Model(inputs=inputs, outputs=outputs)
