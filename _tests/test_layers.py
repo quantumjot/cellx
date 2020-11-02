6ce174a16de343658b9c586a52e199e17fee835e
quantumjot <code@arlowe.co.uk>
2020-11-02 15:35:51 +0000

    added test

--- _tests/test_layers.py
+++ _tests/test_layers.py
@@ -1,9 +1,9 @@
 import pytest
 
-from cellx.layers import ConvBlock2D
+from cellx.layers import ConvBlock2D, ConvBlock3D
 
 
-@pytest.mark.parametrize("layer", [ConvBlock2D])
+@pytest.mark.parametrize("layer", [ConvBlock2D, ConvBlock3D])
 def test_layer_instantiation(layer):
     """Test instantiating network layers."""
     new_layer = layer()
