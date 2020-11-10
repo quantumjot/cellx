9b4d2dd9797176b6cf51ff279d1c64111a575c4e
quantumjot <code@arlowe.co.uk>
2020-11-10 15:36:48 +0000

    updated docstrings

--- cellx/augmentation/utils.py
+++ cellx/augmentation/utils.py
@@ -4,8 +4,8 @@ import tensorflow as tf
 
 
 def augmentation_label_handler(augmentation_fn):
-    """Deals with functions that also return a label, by augmenting the data,
-    but not the label (if present)."""
+    """Decoreator that deals with functions that also return a label, by
+    augmenting the data, but not the label (if present)."""
 
     @wraps(augmentation_fn)
     def _wrapper(*args, **kwargs):
@@ -20,11 +20,26 @@ def augmentation_label_handler(augmentation_fn):
 
 def append_conditional_augmentation(
     dataset: tf.data.Dataset, augmentations: list, accept_probability: float = 0.1
-):
+) -> tf.data.Dataset:
     """Append augmentations to a TF dataset, each with a probability of
-    acceptance."""
+    acceptance.
+
+    Parameters
+    ----------
+    dataset : tf.data.Dataset
+        A tensorflow dataset to be augmented.
+    augmentations : list of functions
+        A list of functions which accept a tf.Tensor and augment it.
+    accept_probability : float
+        A float representing the probability of performing the augmentation.
+
+    Returns
+    -------
+    dataset : tf.data.Dataset
+        The augmented dataset.
+    """
     for augmentation in augmentations:
-        dataset.map(
+        dataset = dataset.map(
             lambda x: tf.cond(
                 tf.random.uniform([], 0.0, 1.0) > accept_probability,
                 lambda: augmentation(x),
