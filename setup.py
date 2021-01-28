94dbd7e5a2e308579859cad8e4410b98a807130d
quantumjot <code@arlowe.co.uk>
2021-01-28 08:35:12 +0000

    updated setup

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import find_packages, setup
 
-setup(name='cellx',
-      version='0.1',
-      description='CellX libraries',
-      author='Alan R. Lowe, Christopher Soelistyo, Laure Ho',
-      author_email='a.lowe@ucl.ac.uk',
-      url='https://github.com/quantumjot/cellx',
-      packages=['cellx'],
-      install_requires=['matplotlib',
-                        'numpy',
-                        'scikit-image',
-                        'scikit-learn',
-                        'scipy',
-                        'tensorflow',
-                        'tqdm'],
-      python_requires='>=3.6')
+setup(
+    name="cellx",
+    version="0.1",
+    description="CellX libraries",
+    author="Alan R. Lowe, Christopher Soelistyo, Laure Ho",
+    author_email="a.lowe@ucl.ac.uk",
+    url="https://github.com/quantumjot/cellx",
+    packages=find_packages(),
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "scikit-image",
+        "scikit-learn",
+        "scipy",
+        "tensorflow",
+        "tqdm",
+    ],
+    python_requires=">=3.6",
+)
