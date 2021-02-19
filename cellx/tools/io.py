4c3b6ebb1a3d57c8aa38e8f4142417338b077883
quantumjot <code@arlowe.co.uk>
2021-02-19 09:55:04 +0000

    Added annotation file reader

--- cellx/tools/io.py
+++ cellx/tools/io.py
@@ -1,6 +1,8 @@
+import enum
 import hashlib
 import json
 import os
+import zipfile
 
 import numpy as np
 from skimage import io
@@ -176,3 +178,85 @@ class EncodingReader:
         stack = np.rollaxis(stack, 1, 4)
 
         return stack, metadata
+
+
+def read_annotations(path: str):
+    """Read annotations.
+
+    This provides a capability to load the contents of a single, or multiple
+    annotation files, aggregating their contents and returning a dictionary of
+    state labels.
+
+    Parameters
+    ----------
+    path : str
+        The path to the folder containing the annotation.zip files
+
+    Returns
+    -------
+    images : np.ndarray
+        An array of N image patches of the format N,(Z),X,Y,C
+    labels : np.ndarray
+        An array of numeric image labels of size (N, )
+    states : dict
+        A dictionary mapping a string label to the numeric image label
+    """
+
+    images = []
+    labels = []
+    states = {}
+
+    # find the zip files:
+    zipfiles = [
+        os.path.join(path, f)
+        for f in os.listdir(path)
+        if f.endswith(".zip") and f.startswith("annotation_")
+    ]
+
+    if len(zipfiles) == 0:
+        raise IOError("Warning, no 'annotation' zip files found.")
+
+    # iterate over the zip files and aggregate the data
+    for zfn in zipfiles:
+
+        with zipfile.ZipFile(zfn, "r") as zip_data:
+            files = zip_data.namelist()
+
+            # first open the annotation file
+            json_fn = [f for f in files if f.endswith(".json")][0]
+            with zip_data.open(json_fn) as js:
+                json_data = json.load(js)
+                _states = json_data["states"]
+
+                # if we have no states defined, use the serialized states to
+                # define them
+                if not states:
+                    states = _states
+                    States = enum.Enum("States", states)
+
+                # if they do exist, make sure that they match all other files
+                if states != _states:
+                    raise Exception("Annotation files are incompatible")
+
+            for state in States:
+
+                numeric_label = state.value
+                label = state.name
+
+                patch_files = [
+                    f for f in files if f.endswith(".tif") and f.startswith(label)
+                ]
+
+                raw_images = [io.imread(zip_data.open(f)) for f in patch_files]
+                # images_resized = [resize(img, (64, 64), preserve_range=True) for img in images]
+
+                images += raw_images
+                labels += [numeric_label] * len(raw_images)
+
+    images_arr = np.stack(images, axis=0)[..., np.newaxis]
+    labels_arr = np.stack(labels, axis=0)
+
+    # sanity check that we have the same number of labels and images
+    assert images_arr.shape[0] == labels_arr.shape[0]
+
+    return images_arr, labels_arr, states
