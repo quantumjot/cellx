18f770749c7919b4ce87acce4bc37407989ebb2c
quantumjot <code@arlowe.co.uk>
2021-04-30 09:07:57 +0100

    update requirements for Docker

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 from setuptools import find_packages, setup
 
+requirements = []
+with open("requirements.txt") as f:
+    for line in f:
+        stripped = line.split("#")[0].strip()
+        if len(stripped) > 0:
+            requirements.append(stripped)
+
 setup(
     name="cellx",
     version="0.1",
@@ -9,14 +16,6 @@ setup(
     author_email="a.lowe@ucl.ac.uk",
     url="https://github.com/quantumjot/cellx",
     packages=find_packages(),
-    install_requires=[
-        "matplotlib",
-        "numpy",
-        "scikit-image",
-        "scikit-learn",
-        "scipy",
-        "tensorflow",
-        "tqdm",
-    ],
+    install_requires=requirements,
     python_requires=">=3.6",
 )
