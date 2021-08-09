6dc53904fb7019c629fe8bb7fb69700b33d1ddfe
quantumjot <code@arlowe.co.uk>
2021-08-09 11:41:08 +0100

    allow additional custom objects when loading models

--- cellx/core.py
+++ cellx/core.py
@@ -18,13 +18,15 @@ def _get_custom_layers_dict() -> dict:
     return custom_objects
 
 
-def load_model(filename: str, **kwargs) -> K.Model:
+def load_model(filename: str, extra_custom_objects: dict = {}, **kwargs) -> K.Model:
     """Convenience function to load a Keras model with custom cellx layers.
 
     Parameters
     ----------
     filename : str
         Filename for the model.
+    extra_custom_objects : dict
+        A dictionary containing any extra custom objects for the model.
 
     Returns
     -------
@@ -32,5 +34,13 @@ def load_model(filename: str, **kwargs) -> K.Model:
         The model.
     """
     custom_objects = _get_custom_layers_dict()
+
+    if not isinstance(extra_custom_objects, dict):
+        raise TypeError(
+            f"`extra_custom_objects` should be a dictionary not "
+            f"{type(extra_custom_objects)}."
+        )
+    custom_objects.update(extra_custom_objects)
+
     model = K.models.load_model(filename, custom_objects=custom_objects, **kwargs)
     return model
