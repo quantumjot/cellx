c535a9ad9df938a85de5b75f7e3ea456e51b4d09
Nathan Day <day.nathan@pm.me>
2021-08-09 18:32:41 +0100

    Update projection.py
    changing rand->random

--- cellx/manifold/projection.py
+++ cellx/manifold/projection.py
@@ -20,7 +20,7 @@ class ProjectionMethod(CallableEnum):
         * SUM - the per-pixel sum of the images in the bin
         * STD - the per-pixel standard deviation of images in the bin
         * FIRST - the first image in the bin
-        * RAND - random image in the bin
+        * RANDOM - random image in the bin
     """
 
     MEAN = partial(np.mean, axis=0)
@@ -29,7 +29,7 @@ class ProjectionMethod(CallableEnum):
     SUM = partial(np.sum, axis=0)
     STD = partial(np.std, axis=0)
     FIRST = partial(lambda x: x[0, ...])
-    RAND = partial(lambda x: (x[np.random.choice(len(x[:,...])), ...]))
+    RANDOM = partial(lambda x: (x[np.random.choice(len(x[:,...])), ...]))
 
 
 class ManifoldProjection2D:
@@ -119,7 +119,7 @@ class ManifoldProjection2D:
                 * SUM - the per-pixel sum of the images in the bin
                 * STD - the per-pixel standard deviation of images in the bin
                 * FIRST - the first image in the bin
-                * RAND - random image in the bin
+                * RANDOM - random image in the bin
 
         Returns
         -------
