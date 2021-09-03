97f59e9c51dd6659f5d084981ba457685ba38b2b
quantumjot <code@arlowe.co.uk>
2021-09-03 12:52:33 +0100

    Allow batch PCA transforms

--- cellx/layers/transform.py
+++ cellx/layers/transform.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras as K
 
@@ -12,13 +13,22 @@ class PCATransform(K.layers.Layer):
     components : np.array, tf.Tensor (N, N)
         A square matrix of principal components to perform the transform. These
         are calculated using `sklearn.decomposition.PCA`.
+
+    Notes
+    -----
+    Assumes that the number of principal components is the same as the number of
+    input features, i.e. that the components matrix is square.
     """
 
-    def __init__(self, components, **kwargs):
+    def __init__(self, components: np.ndarray, **kwargs):
         super().__init__(**kwargs)
         self.components = components
+        self.n_components = components.shape[-1]
 
     def call(self, x, training: Optional[bool] = None):
         """Perform the transformation: T = XW"""
-        T = tf.matmul(x, self.components)
-        return T - tf.math.reduce_mean(T, axis=-2, keepdims=True)
+        T = tf.reshape(
+            tf.matmul(tf.reshape(x, (-1, self.n_components)), self.components),
+            (-1, x.shape[1], self.n_components),
+        )
+        return T - tf.math.reduce_mean(T, axis=1)
