7c582657907563f7ea6c36c2a9b8edd5ea752d5b
quantumjot <code@arlowe.co.uk>
2022-02-15 11:33:49 +0000

    fix saliency

--- cellx/tools/saliency.py
+++ cellx/tools/saliency.py
@@ -1,7 +1,6 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras as K
-from tqdm import tqdm
 
 
 def visualize_saliency(
@@ -21,10 +20,11 @@ def visualize_saliency(
     ----------
     model : Keras.Model
         The trained Keras model.
-    data_wrt : np.ndarray
-        The data to calculate the gradients wrt.
+    data_wrt : np.ndarray, (1, ...)
+        The data to calculate the gradients wrt. Batch size (i.e) first dimension
+        should be one.
     n_samples : int
-        The number of samples to take when generating the saliency.
+        The number of samples to take when generating the saliency. Must be greater than one.
     epsilon : float
         A small offset to prevent divide by zero when normalizing the gradients.
     normalize : bool
@@ -46,39 +46,37 @@ def visualize_saliency(
     Karen Simonyan and Andrea Vedaldi and Andrew Zisserman
     """
 
+    # batch size should only be one
+    assert data_wrt.shape[0] == 1
+    assert n_samples > 0
+
     # SmoothGrad
     stddev = stddev_spread * (np.max(data_wrt) - np.min(data_wrt))
 
-    tensor_wrt = tf.Variable(data_wrt, dtype=tf.float32)
+    # use the batch to perform the sampling
+    tensor_wrt = np.concatenate([data_wrt] * n_samples, axis=0)
+    assert tensor_wrt.ndim == data_wrt.ndim
+    assert tensor_wrt.shape[0] == n_samples
 
     # SmoothGrad
     if n_samples > 1:
+        rng = np.random.default_rng()
+        noise = rng.normal(loc=0, scale=stddev, size=tensor_wrt.shape)
+        tensor_wrt = tensor_wrt + noise
 
-        smooth_gradient = []
-        for sample in tqdm(range(n_samples)):
-            noise = tf.random.normal(data_wrt.shape, mean=0, stddev=stddev)
-            tensor_plus_noise = tensor_wrt + noise
+    # calculate the gradients using the sample
+    tensor_wrt = tf.Variable(tensor_wrt, dtype=tf.float32)
+    with tf.GradientTape() as tape:
+        logits = model(tensor_wrt, training=False)
+        # get the gradient and magnitude of the gradient
+        gradient = tape.gradient(logits, tensor_wrt)
+        square_gradient = tf.square(gradient)
 
-            with tf.GradientTape() as tape:
-                logits = model(tensor_plus_noise, training=False)
-                tape.watch(tensor_plus_noise)
-                gradient = tape.gradient(logits, tensor_plus_noise)
-                # absolute_gradient = np.abs(gradient.numpy())
-                absolute_gradient = tf.square(gradient).numpy()
-
-            smooth_gradient.append(absolute_gradient)
-        absolute_gradient = (
-            np.sum(np.stack(smooth_gradient, axis=0), axis=0) / n_samples
-        )
-
-    # gradients only
-    else:
-        with tf.GradientTape() as tape:
-            logits = model(tensor_wrt, training=False)
-            # get the gradient and magnitude of the gradient
-            gradient = tape.gradient(logits, tensor_wrt)
-
-        absolute_gradient = np.abs(gradient.numpy())
+    absolute_gradient = tf.reduce_mean(
+        square_gradient,
+        axis=0,
+        keepdims=True,
+    ).numpy()
 
     # if we're not normalizing, return the raw gradient magnitude
     if not normalize:
